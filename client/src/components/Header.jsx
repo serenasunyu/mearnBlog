28fcf1fd0c2fb95d867b3b8e0d96ff075b6f1e85
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-10-16 15:38:58 -0400

    add functionality to the sign up page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,74 +1,78 @@
 import React from 'react';
-import {Button, Navbar, TextInput } from 'flowbite-react';
+import { Button, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
-
     const path = useLocation().pathname;
 
-  return (
-    <div>
-        <Navbar className='border-b-2 ml-2 py-2 flex-row justify-between'>
-            <Link
-                to="/"
-                className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-            >
-                <span className="px-2 py-1 mr-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white"> 
-                Yu's 
-                </span>
-                Blog
-            </Link>
-
-            <form className='relative'>
-                <TextInput
-                    type="text"
-                    placeholder="Search..."
-                    // rightIcon={ AiOutlineSearch }
-                    className="hidden lg:inline"
-                />    
-                <AiOutlineSearch className='absolute right-2 top-1/3 text-gray-400 hidden lg:inline' />
-            </form>
-
-            <Button className='w-12 h-10 lg:hidden rounded-full' pill>
-                <AiOutlineSearch className='text-gray-400 transform translate-y-1.5'/>
-            </Button>
-            
-            <div className='flex gap-2 md:order-2 mt-1'>
-                <Button
-                    className='w-12 h-10 hidden sm:flex items-center justify-center mt-1' pill
+    return (
+        <div>
+            <Navbar className="border-b-2 ml-2 py-2 flex-row justify-between">
+                <Link
+                    to="/"
+                    className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
                 >
-                    <FaMoon className='text-black' />
+                    <span className="px-2 py-1 mr-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                        Yu's
+                    </span>
+                    Blog
+                </Link>
+
+                {/* Search Bar */}
+                <form>
+                    <TextInput
+                        type="text"
+                        placeholder="Search..."
+                        rightIcon={AiOutlineSearch}
+                        className="hidden lg:inline"
+                    />
+                    {/* <AiOutlineSearch className="absolute right-2 top-1/3 text-gray-400" /> */}
+                </form>
+
+                {/* Search Icon for Small Screens */}
+                <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+                    <AiOutlineSearch />
                 </Button>
 
-                <Link to="/sign-in" className='mb-2'>
-                    <Button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-3 text-center py-1 me-2">
-                    
-                        Sign In 
+                {/* Right side: Buttons and Toggle */}
+                <div className="flex gap-2 md:order-2 mt-1">
+                    <Button className="w-12 h-10" color='grey' pill>
+                        <FaMoon />
                     </Button>
-                </Link>
-                <Navbar.Toggle aria-label="Toggle navigation" className='lg:hidden'/>
 
-            </div>
+                    <Link to="/sign-in" className="mb-2">
+                        <Button gradientDuoTone='purpleToBlue' outline>
+                            Sign In
+                        </Button>
+                    </Link>
 
-            <Navbar.Collapse className='lg:flex lg:justify-evenly'>
-                <Navbar.Link active={path === "/"} as={"div"}>
-                    <Link to="/" className='text-black hover:text-blue-500 mr-6'> Home </Link>
-                </Navbar.Link>
+                    {/* Toggle for Mobile */}
+                    <Navbar.Toggle/>
+                </div>
 
-                <Navbar.Link active={path === "/about"} as={"div"}>
-                    <Link to="/about" className='text-black hover:text-blue-500 mr-6'> About </Link>
-                </Navbar.Link>
+                {/* Collapsible Menu */}
+                <Navbar.Collapse className="lg:flex lg:justify-evenly">
+                    <Navbar.Link active={path === "/"} as="div">
+                        <Link to="/" className="text-black hover:text-blue-500 mr-6">
+                            Home
+                        </Link>
+                    </Navbar.Link>
 
-                <Navbar.Link active={path === "/articles"} as={"div"}>
-                    <Link to="/articles" className='text-black hover:text-blue-500'> Articles </Link>
-                </Navbar.Link>
-            </Navbar.Collapse>
+                    <Navbar.Link active={path === "/about"} as="div">
+                        <Link to="/about" className="text-black hover:text-blue-500 mr-6">
+                            About
+                        </Link>
+                    </Navbar.Link>
 
-            
-        </Navbar>
-
-    </div>
-  )
+                    <Navbar.Link active={path === "/articles"} as="div">
+                        <Link to="/articles" className="text-black hover:text-blue-500">
+                            Articles
+                        </Link>
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
