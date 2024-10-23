b63bc9f5ee6ad904e3d8fa7d5bdc0a2a8e6d137c
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-10-23 00:53:44 -0400

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,6 +6,8 @@ export const test = (req, res) => {
     res.json({ message: 'API is working!' });
 };
 
+
+// Update user profile
 export const updateUser = async (req, res, next) => {
     try {
         // Check if the logged-in user is allowed to update the given user ID
@@ -60,6 +62,21 @@ export const updateUser = async (req, res, next) => {
 
         // Respond with the updated user data
         res.status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+//  Delete user account
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User has been deleted');
 
     } catch (error) {
         next(error);
