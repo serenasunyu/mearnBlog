fe67c52333bd12d731443aa86823d9215a7c356b
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-10-23 01:53:49 -0400

    add signout functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,10 +2,13 @@ import { Sidebar } from 'flowbite-react';
 import { HiUser, HiArrowSmRight} from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function DashSidebar() {
     const location = useLocation();
     const [tab, setTab] = useState('');
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -14,6 +17,24 @@ export default function DashSidebar() {
         setTab(tabFromUrl);
         }
     }, [location.search]);
+
+    const handleSignout = async () => {
+        try {
+            const res = await fetch('/api/user/signout', {
+                method: 'POST',
+            });
+
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     
     return (
         <Sidebar className='w-full md:w-56'>
@@ -31,7 +52,11 @@ export default function DashSidebar() {
                         Profile
                     </Sidebar.Item>
 
-                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' >
+                    <Sidebar.Item 
+                        icon={HiArrowSmRight} 
+                        className='cursor-pointer' 
+                        onClick={handleSignout}
+                    >
                         Sign Out
                     </Sidebar.Item>
 
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -50,6 +50,11 @@ const userSlice = createSlice ({
             state.loading = false;
             state.error = action.payload;
         },
+        signoutSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        }
     },
 });
 
@@ -63,5 +68,6 @@ export const {
     deleteUserStart,
     deleteUserSuccess,
     deleteUserFailure,
+    signoutSuccess,
 } = userSlice.actions;
 export default userSlice.reducer;
