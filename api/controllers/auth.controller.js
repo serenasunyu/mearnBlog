f5350d30215cf26b0168ed7a214700a568e798ed
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-10-28 15:12:08 -0400

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -56,7 +56,7 @@ export const signin = async(req, res, next) => {
         }
 
         const token = jwt.sign(
-            { id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d'}
+            { id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d'}
         ); 
 
         const { password: pass, ...rest } = validUser._doc;
@@ -82,7 +82,7 @@ export const google = async (req, res, next) => {
         const user = await User.findOne({ email });
         if (user) {
             const token = jwt.sign(
-                {id: user._id}, 
+                {id: user._id, isAdmin: user.isAdmin }, 
                 process.env.JWT_SECRET
             );
             const {password, ...rest} = user._doc;
@@ -108,7 +108,7 @@ export const google = async (req, res, next) => {
 
             await newUser.save();
             const token = jwt.sign(
-                    { id: newUser._id }, 
+                    { id: newUser._id, isAdmin: newUser.isAdmin }, 
                     process.env.JWT_SECRET
                 );
             const {password, ...rest} = newUser._doc;
