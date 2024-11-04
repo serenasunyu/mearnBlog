0dd3261bbdad1a42bc91499aa2c9383bd8e7f7a8
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-11-04 00:09:39 -0500

    add show more functionality  the post list of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const {currentUser} = useSelector(state => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
 
   useEffect(() => {
@@ -16,6 +17,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
         console.log(data);
 
@@ -29,7 +33,23 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
 
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
 
   return (
@@ -90,6 +110,17 @@ export default function DashPosts() {
             ))}
 
           </Table>
+
+          {
+            showMore && (
+              <button
+                onClick={handleShowMore}
+                className="w-full text-teal-500 self-center text-sm py-7">
+                Show more
+              </button>
+            ) 
+          }
+
         </>
       ) : (
         <p>You have no posts yet!</p>
