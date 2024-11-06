555bc6c8d018f308c3243491ea94aa36e53922a8
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-11-05 22:51:29 -0500

    complete post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute copy";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 
 export default function App() {
@@ -30,6 +31,7 @@ export default function App() {
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/articles" element={<Articles />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
         
       </Routes>
       <Footer />
