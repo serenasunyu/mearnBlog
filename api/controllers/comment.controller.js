e4e43c847070b69f24182790794ff9bffa9f1dd4
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-11-07 14:28:43 -0500

    show the comments of the post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -19,6 +19,18 @@ export const createComment = async(req, res, next) => {
         res.status(200).json(newComment);
 
     } catch (error) {
-        next(error)
+        next(error);
+    }
+}
+
+export const getPostComments = async(req, res, next) => {
+    try {
+        const comments = await Comment
+                                .find({ postId: req.params.postId })
+                                .sort({ createdAt: -1, });
+        res.status(200).json(comments);
+
+    } catch (error) {
+        next(error);
     }
 }
